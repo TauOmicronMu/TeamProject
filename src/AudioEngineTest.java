71a073bd4b2e181ef7115c911971f3daf0a9f4e7
tauomicronmu <tauomicronmurs@gmail.com>
2017-02-10 15:38:11 +0000

    small change to the test

--- src/AudioEngineTest.java
+++ src/AudioEngineTest.java
@@ -11,12 +11,9 @@ public class AudioEngineTest {
     public static void main(String[] args) {
         AudioEngine engine = new AudioEngine();
         int ref = engine.createTrack("meow.wav");
-        int ref2 = engine.createTrack("meow.wav");
+        engine.playTrack(ref);
         engine.loopTrack(ref);
-        for(int i = 0; i < 20; i++) {
-            sleep(250);
-            engine.playTrack(ref2);
-        }
-        while(true);
+        sleep(5000);
+        engine.stopTrack(ref);
     }
 }
