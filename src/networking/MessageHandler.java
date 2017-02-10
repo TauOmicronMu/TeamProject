9a3d719fe8c4edef75133ed6fc31a2b031c330fe
Bede Kelly <bedekelly97@gmail.com>
2017-02-10 08:07:38 +0000

    Add info to MessageHandler docstring

--- src/networking/MessageHandler.java
+++ src/networking/MessageHandler.java
@@ -1,7 +1,10 @@
 package networking;
 
 /**
- * This MessageHandler interface exists for two reasons:
+ * This MessageHandler interface exists to ensure that anything extending
+ * NetworkUser (an *abstract* class) must define a handleMessage method.
+ * This method is called whenever we receive a message on a socket, and
+ * is essential for both the client and server to implement.
  */
 interface MessageHandler {
     void handleMessage(Message m);
