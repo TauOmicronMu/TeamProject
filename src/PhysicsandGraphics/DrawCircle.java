3f618ac6226d680fe8538656a046db608b31aaa7
Jennifer Kwan <Jennifer Kwan>
2017-02-12 10:58:25 +0000

    menu integration

---
+++ src/PhysicsandGraphics/DrawCircle.java
@@ -0,0 +1,64 @@
+package PhysicsandGraphics;
+
+import static org.lwjgl.opengl.GL11.glColor4f;
+
+public class DrawCircle {
+	
+	public DrawCircle(){
+		
+	}
+	
+	/**
+	 * Draws the pinball
+	 * @param posx the current x position of the centre of the pinball
+	 * @param posy the current y position of the centre of the pinball
+	 */
+	public void paintPinball(Graphics game, int x, int y)
+	{
+		//System.out.println("Old X: " + x);
+		//System.out.println("New X: " + game.changexCoord(x));
+		float[] vertices = createCircle(game.changexCoord(x),game.changeyCoord(y),0.5f,0.08f);
+		Model circle1 = new Model(vertices);
+		circle1.render(vertices);
+	}
+	
+	public void paintItem(Graphics game, int x, int y)
+	{
+		float[] vertices = createCircle(game.changexCoord(x),game.changeyCoord(y),0.3f,0.03f);
+		Model circle1 = new Model(vertices);
+		circle1.render(vertices);
+	}
+	
+	/**
+	 * Calculates all the points of the circumference of the circle
+	 * @param posx the current x position of the centre of the pinball
+	 * @param posy the current y position of the centre of the pinball
+	 * @param posz the current z position of the centre of the pinball
+	 * @return all the points of the circle
+	 */
+	private static float[] createCircle(float posx, float posy, float posz, double radius)
+	{
+		int noSides = 360;
+		int noVertices = noSides + 2;
+		float doublePI = (float) Math.PI * 2;
+		
+		int i = 1;
+		float[] vertices = new float[noVertices*3];
+		float x = posx;
+		float y = posy;
+		float z = posz;
+		vertices[0] = x;
+		vertices[1] = y;
+		vertices[2] = z;
+		for(int j= 3; j < (noVertices * 3); j = j + 3)
+		{			
+			vertices[j] = (float) ( x + (radius * Math.cos(i * doublePI / noSides)));
+			vertices[j+1] = (float) ( y + (radius * Math.sin(i * doublePI / noSides)));
+			vertices[j+2] = z;
+			i++;
+		}
+
+		return vertices;
+	}
+
+}
---
+++ src/PhysicsandGraphics/PlatformE.java
@@ -0,0 +1,35 @@
+package PhysicsandGraphics;
+
+public class PlatformE {
+
+	/**
+	 * Once the vertices have been created, the obstacle is drawn
+	 * @param rnumx the top left x coordinate
+	 * @param rnumy the top left y coordinate
+	 * @param rsign which half (top or bottom) of the screen the obstacle should be in
+	 */
+	public static void drawobstacle(float rnumx, float rnumy, float rsigny, float rsignx) 
+	{	
+		if(rsigny==1 && rsignx ==1)
+		{					
+			float[] verticesb = {rnumx,rnumy,0.3f,rnumx,(rnumy - 0.05f),0.3f,(rnumx - 0.3f),(rnumy - 0.05f),0.3f,(rnumx -0.3f),rnumy,0.3f};
+			Rectangle.drawrectangle(verticesb);
+		}
+		if(rsigny==1 && rsignx ==0)
+		{
+			float[] verticesb = {-rnumx,rnumy,0.3f,-rnumx,(rnumy - 0.05f),0.3f,(-rnumx + 0.3f),(rnumy - 0.05f),0.3f,(-rnumx +0.3f),rnumy,0.3f};
+			Rectangle.drawrectangle(verticesb);
+		}
+		if(rsigny==0 && rsignx ==1)
+		{
+			float[] verticesb = {rnumx,-rnumy,0.3f,rnumx,(-rnumy + 0.05f),0.3f,(rnumx - 0.3f),(-rnumy + 0.05f),0.3f,(rnumx -0.3f),-rnumy,0.3f};
+			Rectangle.drawrectangle(verticesb);
+		}
+		if(rsigny==0 && rsignx ==0)
+		{
+			float[] verticesb = {-rnumx,-rnumy,0.3f,-rnumx,(-rnumy + 0.05f),0.3f,(-rnumx + 0.3f),(-rnumy + 0.05f),0.3f,(-rnumx + 0.3f),-rnumy,0.3f};
+			Rectangle.drawrectangle(verticesb);
+		}
+			
+	}
+}
