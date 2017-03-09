684384fd32bb4a6026b18096503ac0ed9e779821
csc551 <csc551@student.bham.ac.uk>
2017-03-09 18:11:46 +0000

    Update EDA.java

--- EDA.java
+++ EDA.java
@@ -1,19 +1,17 @@
-package teamproject;
-import org.apache.commons.math3.linear.*;
+package main;
+
+//import org.apache.commons.math3.linear.*;
 import org.apache.commons.math3.stat.*; 
 import org.apache.commons.math3.distribution.*;
-import java.util.function.Function;
+//import java.util.function.Function;
 import java.util.ArrayList;
-import java.util.Arrays;
+//import java.util.Arrays;
 
 public class EDA {
-	private final int A = 100; // number of NEW samples per iteration: angel
-	private final int P = 5;
-    private final int Dim = 2; // dimension of vectors: Power & Angle
+	private final int xv = 100; // number of NEW samples per iteration: x-velocity
     
     private ArrayList<Double> score = new ArrayList<>(); //store score which show where the ball hit
-    private ArrayList<Double> angle = new ArrayList<>(); //angle corresponding to score list
-    private ArrayList<Double> power = new ArrayList<>(); //power corresponding to score list 
+    private ArrayList<Double> xVelocity = new ArrayList<>(); //x-velocity corresponding to score list
     
     //Getter//
     public EDA(){
@@ -25,80 +23,79 @@ public class EDA {
         return score;
     }
     
-    public ArrayList<Double> getAngle(){
-    	return angle;
+    public ArrayList<Double> getXVelcoity(){
+    	return xVelocity;
     }
     
-    public ArrayList<Double> getPower(){
-    	return power;
-    }
-    
-    /*
-    public ArrayList<Double[]> getAnglePower(){
-    	return anglePower;
-    }*/
-
-    public double simpleEDA(toScoreFunction f,double[] a, int time){//time: times of iteration
+    /**
+     * generate a EDA
+     * @param f a function that returns score which shows where the ball hit.
+     * @param xV range of x-velocity
+     * @param time time of iteration, more times will returns a better result
+     * @return a best x-velocity of specify x and y distance
+     */
+    public double simpleEDA(toScore f,double[] xV, int time, int x, int y){
     	int iterationCounter = 0;//count how many time of iteration it did
-    	//double[] keepAngle = a;
-    	ArrayList<Double> keepAngle = new ArrayList<Double>();
-    	for(int i = 0; i < a.length-1; i++){
-    		keepAngle.add(a[i]);
+    	ArrayList<Double> keepSample = new ArrayList<Double>();
+    	for(int i = 0; i < xV.length-1; i++){
+    		keepSample.add(xV[i]);
     	}
-    	/*
-    	double[] temp = new double[keepAngle.size()];
-    	for(int i = 0; i < keepAngle.size()-1;i++){
-    		temp[i] = keepAngle.get(i);
-    	}
-    	*/
-    	while (iterationCounter < time)
-    	{
-    		double meanD = mean(keepAngle);
-            double varianceD = variance(keepAngle) % keepAngle.size(); 
-            //NormalDistribution M = new NormalDistribution(meanD,varianceD); //Normal distribution of parameter
+ 
+    	while (iterationCounter < time){
+    		double meanD = mean(keepSample);
+            double varianceD = variance(keepSample)+5; //if maximum x velocity is bigger
             NormalDistribution M = new NormalDistribution(meanD,varianceD); //Normal distribution of parameter
-            ArrayList<Double> sampleArray = sampleRange(M.sample(A)); // choose S samples angle from M distribution
-
+            ArrayList<Double> sampleArray = sampleRange(M.sample(xv)); // choose S samples from M distribution
             ArrayList<Double> result = f.apply(sampleArray); // apply sample into function f
-        
-            System.out.println("Sample Angle:");
-            printArray(sampleArray);
-            System.out.println("Result:");
-            printArray(result);
+            if(result == null){
+            	return -1.0;
+            }
+//            System.out.println("Sample:");
+//            printArray(sampleArray);
+//            System.out.println("Result:");
+//            printArray(result);
             
-            keepAngle = new ArrayList<Double>();
+            keepSample = new ArrayList<Double>();
             double meanResult = mean(result);
             int maxIndex = 0;
             for(int i = 0; i < result.size(); i++){
             	if(result.get(i) > meanResult){
-            		keepAngle.add(sampleArray.get(i));
+            		keepSample.add(sampleArray.get(i));
             	}
             	if(result.get(i) > result.get(maxIndex)){
             		maxIndex = i;
             	}
             }
             
-            System.out.println("Next sample:");
-            printArray(keepAngle);
-            
+//            System.out.println("Next sample:");
+//            printArray(keepSample);
+//            System.out.println("MaxIndex: " + maxIndex + " Result size: " + result.size());
+            if(result.size() == 0){
+            	return -1.0;
+            }
             score.add(result.get(maxIndex));
-            angle.add(sampleArray.get(maxIndex));
+            xVelocity.add(sampleArray.get(maxIndex));
             iterationCounter++;
     	}
-    	int bestAngle = 0;//index of angle
+    	int bestXV = 0;//index of x-velocity
     	for(int i = 0; i < score.size(); i++){
-    		if(score.get(i) > score.get(bestAngle)){
-    			bestAngle = i;
+    		if(score.get(i) > score.get(bestXV)){
+    			bestXV = i;
     		}
     	}
-    	
-    	return angle.get(bestAngle);
+//    	System.out.println(xVelocity.get(bestXV));
+    	return xVelocity.get(bestXV);
     }
     
+    /**
+     * Remove badly sample
+     * @param a ArrayList of new sample
+     * @return an ArrayList of good sample
+     */
     private ArrayList<Double> sampleRange(double[] a){
     	ArrayList<Double> result = new ArrayList<Double>();
     	for(int i = 0; i < a.length -1 ; i++){
-    		if(a[i] > 0 && a[i] <= 180){
+    		if(a[i] >= -5 && a[i] <= 5){
     			result.add(a[i]);
     		}
     	}
@@ -123,7 +120,7 @@ public class EDA {
     
     private void printArray(ArrayList<Double> l){
     	for(int i = 0; i < l.size(); i++){
-    		System.out.println(l.get(i));
+    		System.out.println(i + ": " + l.get(i));
     	}
     }
 }
