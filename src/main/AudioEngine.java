9536f191468dfe2b25e9eda808bdb5d3ad1dfb9c
TauOmicronMu <TauOmicronMuRS@Gmail.com>
2017-03-21 06:46:40 +0000

    recorded grav up/down sounds

--- src/main/AudioEngine.java
+++ src/main/AudioEngine.java
@@ -21,6 +21,8 @@ public class AudioEngine {
     // Set up the sounds for the AudioEngine
     public static final int BLOP = AUDIO_ENGINE.createTrack("blopalop.wav");
     public static final int WHOOSH = AUDIO_ENGINE.createTrack("hswoosh.wav");
+    public static final int GRAVDOWN = AUDIO_ENGINE.createTrack("gravdown.wav");
+    public static final int GRAVUP = AUDIO_ENGINE.createTrack("gravup.wav");
     // We don't want to play if we're in the client!
     public static boolean isClient = true;
 
