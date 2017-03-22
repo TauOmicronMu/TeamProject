a0b4be2fd36eea617b9c8073894f6566ad762e9a
TauOmicronMu <TauOmicronMuRS@Gmail.com>
2017-03-22 15:37:44 +0000

    changed AIPlayer to work with new AI interface

--- src/main/AIPlayer.java
+++ src/main/AIPlayer.java
@@ -1,5 +1,7 @@
 package main;
 
+import ai.AI;
+
 import java.io.IOException;
 import java.util.Optional;
 
@@ -10,23 +12,21 @@ public class AIPlayer implements Player {
 
     AIPlayer() {
         super();
-        try {
-            this.ai = new AI();
-        } catch (IOException | ClassNotFoundException e) {
-            System.err.println("[ERROR] AIPlayer constructor : Error while creating AI player.");
-        }
+        this.ai = new AI();
     }
 
     @Override
     public Optional<String> getMove() {
         if (gs == null) return Optional.empty();
-        double xVel = ai.apply(gs.getBall(), gs.getBasicPlatforms());
-        System.out.println("[INFO] AIPlayer.getMove : Requested new X velocity is " + xVel);
-        if (xVel < -gs.getBall().getMaxSpeed()/2)
-            return Optional.of("a");
-        if (xVel > gs.getBall().getMaxSpeed()/2)
-            return Optional.of("d");
-        return Optional.empty();
+        AI.Move move = ai.getMove(gs);
+        switch(move) {
+            case LEFT:
+                return Optional.of("a");
+            case RIGHT:
+                return Optional.of("d");
+            default:
+                return Optional.empty();
+        }
     }
 
     @Override
