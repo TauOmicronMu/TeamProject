0c7a144bc5d9a8e1538d5181100ee4934c983431
csc551 <csc551@student.bham.ac.uk>
2017-03-22 09:49:36 +0000

    Update and rename Testing.java to TestingAI.java

---
+++ TestingAI.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.io.IOException;
+import java.util.Hashtable;
+
+public class TestingAI {
+	public static void main(String args[]) throws ClassNotFoundException, IOException, InterruptedException{
+		GameState game = new GameState(800,800);
+		AIEngine temp = new AIEngine(game);  //run this at the very beginning
+		
+	}
+}
