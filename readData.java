48c587211fd7281ec56751c2abbd1ea160bfc28f
csc551 <csc551@student.bham.ac.uk>
2017-03-22 09:45:35 +0000

    Update readData.java

--- readData.java
+++ readData.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.util.Arrays;
 import java.util.Hashtable;
 
 public class readData {
@@ -8,6 +9,9 @@ public class readData {
 		
 	}
 	
+	private Platform p;
+	private boolean right = true;//platform on right hand side
+	
 	/**
 	 * Input a ball and a list of platform, then return a x-velocity
 	 * @param b Ball
@@ -15,28 +19,56 @@ public class readData {
 	 * @return x-velocity
 	 */
 	public double AI(Ball b, Platform[] ps, Hashtable database,GameState game){
-		Platform p = new Platform();
-		SelectPlatform sp = new SelectPlatform(game);
-		boolean r = true;//platform on right hand side
-		p = sp.select(b,ps);
 		
-		double dx = p.getX() - b.getX();//x-distance between ball and platform 
-		if(dx < -70){
-			r = false;
-			dx = -(dx+140);
-		}
-		double dy = Math.abs(b.getY() - p.getY());//y-distance between ball and platform 
-		double vy = b.getDy();
+//        int ballX = (int) b.getX();
+//        int ballY = (int) b.getY();
+//        int radius = b.getRadius();
+//        
+//        if(!Arrays.asList(ps).contains(game.selected)){
+//        	game.selected = null;
+//        }
+//        
+//        if(game.selected != null){
+//            if (ballY + radius > game.selected.y && ballY + radius < game.selected.y + game.selected.height) {
+//                if (ballX > game.selected.x && ballX < game.selected.x + game.selected.width) {
+//                	System.out.println("Select platform");
+//        			SelectPlatform sp = new SelectPlatform(game);
+//        			game.selected = sp.select(b,ps);
+//                }
+//            }
+//        }else{
+//        	System.out.println("Select platform");
+//			SelectPlatform sp = new SelectPlatform(game);
+//			game.selected = sp.select(b,ps);
+//        }
+//        
+//        p = game.selected;
+		
+		double dx;
+		double dy;
+		double vy;
+		
+			SelectPlatform sp = new SelectPlatform(game);
+			p = sp.select(b,ps);
+			
+			dx = p.x - b.getX();//x-distance between ball and platform 
+			if(dx < -(p.width)/2){
+				right = false;
+				dx = -(dx+p.width);
+			}
+			dy = Math.abs(b.getY() - p.getY());//y-distance between ball and platform 
+			vy = b.getDy();
+		
 		
 		
 		//Score function is symmetric and domain connected.
 		//Straight line between 2 points are inside the domain.
-		//Therefore, we can learn int case and interpolate non-int result(between two int) with convex convernation.		
-		int dx_int = (int)Math.floor(dx);
-		double dx_dec = (10*dx - 10*dx_int)/10;
+		//Therefore, we can learn int(10) case and interpolate non-int result(between two int) with convex convernation.		
+		int dx_int = (int) (dx-(dx%10));
+		double dx_dec = (dx%10)/10;
 		double vector_x = (dx_dec-0.5)/Math.cos(45);
-		int dy_int = (int)Math.floor(dy);
-		double dy_dec = (10*dy - 10*dy_int)/10;
+		int dy_int = (int) (dy-(dy%10));
+		double dy_dec = (dy%10)/10;
 		double vector_y = (dy_dec-0.5)/Math.cos(45);
 		int vy_int = (int)Math.floor(vy);
 		double vy_dec = (10*vy - 10*vy_int)/10;
@@ -44,16 +76,29 @@ public class readData {
 		//Temp method
 		if(vy_dec > 0.5) vy_int++;
 		
-		double result1 = -1.0;
-		double result2 = -1.0;
-		double result3 = -1.0;
-		double result4 = -1.0;
+		double result1 = 0.0;
+		double result2 = 0.0;
+		double result3 = 0.0;
+		double result4 = 0.0;
 		if(database.get(dx_int + "," + dy_int + "," + vy_int) != null)	result1 = (double) database.get(dx_int + "," + dy_int + "," + vy_int);
-		if(database.get((dx_int+1) + "," + dy_int + "," + vy_int) != null)	result2 = (double) database.get((dx_int+1) + "," + dy_int + "," + vy_int);
-		if(database.get(dx_int + "," + (dy_int+1) + "," + vy_int) != null)	result3 = (double) database.get(dx_int + "," + (dy_int+1) + "," + vy_int);
-		if(database.get(dx_int + "," + (dy_int+1) + "," + vy_int) != null)	result4 = (double) database.get(dx_int + "," + (dy_int+1) + "," + vy_int);
+		if(database.get((dx_int+10) + "," + dy_int + "," + vy_int) != null)	result2 = (double) database.get((dx_int+10) + "," + dy_int + "," + vy_int);
+		if(database.get(dx_int + "," + (dy_int+10) + "," + vy_int) != null)	result3 = (double) database.get(dx_int + "," + (dy_int+10) + "," + vy_int);
+		if(database.get((dx_int+10) + "," + (dy_int+10) + "," + vy_int) != null)	result4 = (double) database.get((dx_int+10) + "," + (dy_int+10) + "," + vy_int);
 		double result = ((vector_x+0.5)*result4 + (vector_y+0.5)*result2 + (0.5-vector_x)*result1 + (0.5-vector_y)*result3)/2;
-		if(!r) result = -result;
-		return result;
+		if(result==0 && dx>300) result = 5.0;
+		if(!right) result = -result;
+//		if(vy<0) result = -result;
+		int isb = (int)Math.round(result);
+		double currentDx = b.getDx();
+		
+		if(currentDx > isb){
+			return -1;
+		}else if(currentDx < isb){
+			return 1;
+		}else{
+			return 0;
+		}
+			
+//		return isb;
 	}
 }
